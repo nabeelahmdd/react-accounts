253cb8201bda09b2dbad8a7563c68297517a3827
nabeel.ahmed <nabeel.ahmed@indianic.com>
2023-06-10 15:09:44 +0530

    header nav chnage if user login

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,12 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -18,12 +21,21 @@ function Header() {
             <Nav.Link href="#contact-us">Contact us</Nav.Link>
           </Nav>
           <Nav>
-            <LinkContainer to="/regiser">
-              <Nav.Link>Register</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+            {userInfo ? (
+              <>
+                <Nav.Link>My Account</Nav.Link>
+                <Nav.Link>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/regiser">
+                  <Nav.Link>Register</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
